041b1857ae7d82952d34a61e00d9168c4b277fe9
Alex Kontos <MrAlex94@users.noreply.github.com>
2026-06-29 10:01:36 +0100

    feat(theme): apply Waterfox palettes to content UI

--- browser/components/DesktopActorRegistry.sys.mjs
+++ browser/components/DesktopActorRegistry.sys.mjs
@@ -565,6 +565,7 @@ let JSWINDOWACTORS = {
     includeChrome: true,
     allFrames: true,
     matches: [
+      "about:addons",
       "about:asrouter",
       "about:home",
       "about:newtab",
@@ -583,6 +584,7 @@ let JSWINDOWACTORS = {
       "about:deleteprofile",
       "about:newprofile",
       "about:opentabs",
+      "about:preferences",
       "about:aichatcontent",
     ],
     remoteTypes: ["parent", "privilegedabout"],
