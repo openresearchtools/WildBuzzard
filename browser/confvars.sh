c986a396733c786cc092a4ebb68095e3a2c15d13
openresearchtools <openresearchtools@gmail.com>
2026-08-01 00:25:28 +0100

    feat(product): establish WildBuzzard identity and offline service policy

--- browser/confvars.sh
+++ browser/confvars.sh
@@ -3,11 +3,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# MOZ_APP_DISPLAYNAME will be set by branding/configure.sh
-# MOZ_BRANDING_DIRECTORY is the default branding directory used when none is
-# specified. It should never point to the "official" branding directory.
-# For mozilla-beta, mozilla-release, or mozilla-central repositories, use
-# "unofficial" branding.
-# For the mozilla-aurora repository, use "aurora".
-MOZ_BRANDING_DIRECTORY=browser/branding/unofficial
-MOZ_OFFICIAL_BRANDING_DIRECTORY=browser/branding/official
+# WildBuzzard uses one project-owned branding directory for all local and
+# reproducible builds. No Mozilla or Waterfox product artwork is selected.
+MOZ_BRANDING_DIRECTORY=wildbuzzard/browser/branding
+MOZ_OFFICIAL_BRANDING_DIRECTORY=wildbuzzard/browser/branding
---
+++ wildbuzzard/browser/branding/locales/en-US/brand.ftl
@@ -0,0 +1,11 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+-brand-shorter-name = WildBuzzard
+-brand-short-name = WildBuzzard
+-brand-shortcut-name = WildBuzzard
+-brand-full-name = WildBuzzard
+-brand-product-name = WildBuzzard
+-vendor-short-name = WildBuzzard
+trademarkInfo = { " " }
