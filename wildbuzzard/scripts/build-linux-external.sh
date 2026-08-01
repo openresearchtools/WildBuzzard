4b8ba72dfe6b8918030867f1286b2aad1fbafb87
openresearchtools <openresearchtools@gmail.com>
2026-08-01 10:53:09 +0100

    test(runner): isolate browser suites from desktop focus

--- wildbuzzard/scripts/build-linux-external.sh
+++ wildbuzzard/scripts/build-linux-external.sh
@@ -144,8 +144,8 @@ run_step() {
 run_blocker_tests() {
   run_step blocker-xpcshell-tests ./mach xpcshell-test \
     browser/components/blocker/test/unit/xpcshell.toml
-  run_step blocker-browser-tests ./mach mochitest --flavor browser \
-    browser/components/blocker/test/browser/browser.toml
+  run_step blocker-browser-tests env MOZ_HEADLESS=1 ./mach mochitest \
+    --flavor browser browser/components/blocker/test/browser/browser.toml
 }
 
 run_product_tests() {
@@ -157,8 +157,8 @@ run_product_tests() {
     relative_manifest="${manifest#"${checkout_dir}/"}"
     component="${relative_manifest#wildbuzzard/browser/components/}"
     component="${component%%/*}"
-    run_step "product-browser-${component}" ./mach mochitest \
-      --flavor browser "${relative_manifest}"
+    run_step "product-browser-${component}" env MOZ_HEADLESS=1 \
+      ./mach mochitest --flavor browser "${relative_manifest}"
   done < <(
     find "${checkout_dir}/wildbuzzard/browser/components" \
       -path '*/test/browser/browser.toml' -print | sort
