a58798bd49ebe6aff0211f3b51dac01b145f5183
openresearchtools <229047507+openresearchtools@users.noreply.github.com>
2026-08-31 13:12:02 +0100

    Prepare vendored Cargo sources for gkrust

--- wildbuzzard/scripts/build-linux-external.sh
+++ wildbuzzard/scripts/build-linux-external.sh
@@ -368,6 +368,7 @@ case "${action}" in
     ;;
   gkrust)
     run_step configure ./mach configure
+    run_step pre-export ./mach build pre-export
     run_step gkrust ./mach build \
       toolkit/library/rust/force-cargo-library-build
     ;;
